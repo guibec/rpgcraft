57b218d25edeb327e14f47839810c6d550152f2c
Robin Lavallee <rlavalle@twitch.tv>
2017-02-24 22:30:29 -0800

    after unity upgrade. fail at run

--- unity/Assets/Scripts/Game/Managers/GameManager.cs
+++ unity/Assets/Scripts/Game/Managers/GameManager.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net;
+using UnityEngine.Profiling;
 
 using AdvancedInspector;
 using UnityEngine.SceneManagement;
