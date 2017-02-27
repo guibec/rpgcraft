2753a8422a5b17d054b226c20fed135954b2df05
Robin Lavallee <rlavalle@twitch.tv>
2017-02-27 11:25:04 -0800

    battle mode mock up

---
+++ unity/Assets/Scripts/Game/Battle/BattleManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour {
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    	
+	}
+
+    void UpdatePlayer(Player player) {
+        player.HealthComponent.
+    }
+}
