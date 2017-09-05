95100e870032c66abb8a1e01186d45db1795973b
Jake Stine <Jake.Stine@gmail.com>
2017-09-05 08:25:15 -0700

    DbgConsole WIP - more Lua tie-ins, refactored to utilize int2 vector type

--- dyn-spline/scripts/DbgConsole.lua
+++ dyn-spline/scripts/DbgConsole.lua
@@ -1,5 +1,10 @@
 
 DbgConsole = {}
 
-DbgConsole.Texture  = "..\\8x8font.png"
-DbgConsole.CharSize = { x=8, y=8 }
+DbgConsole.Texture  	= "..\\8x8font.png"
+DbgConsole.CharSize 	= { 8, 8 }
+DbgConsole.SheetSize 	= { 1280, 960 }	-- 1280 for 4:3 ratio console, 960 to leave some margin at the bottom.
+
+DbgConsole.Shader	 	= { "DbgFont.fx", "VS", "FS" }
+
+-- DbgConsole.Shader	 	= { Filename="DbgFont.fx", CallVS="VS", CallFS="FS" }
