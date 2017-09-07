0d940e3a581109cb48dfd6af70648c6e26f5ae35
Jake Stine <Jake.Stine@gmail.com>
2017-09-07 07:54:36 -0700

    DbgFont - bind shader config from lua

--- dyn-spline/scripts/DbgConsole.lua
+++ dyn-spline/scripts/DbgConsole.lua
@@ -5,6 +5,6 @@ DbgConsole.Texture  	= "..\\8x8font.png"
 DbgConsole.CharSize 	= { 8, 8 }
 DbgConsole.SheetSize 	= { 1280, 960 }	-- 1280 for 4:3 ratio console, 960 to leave some margin at the bottom.
 
-DbgConsole.Shader	 	= { "DbgFont.fx", "VS", "FS" }
+DbgConsole.Shader	 	= { "DbgFont.fx", "VS", "PS" }
 
 -- DbgConsole.Shader	 	= { Filename="DbgFont.fx", CallVS="VS", CallFS="FS" }
