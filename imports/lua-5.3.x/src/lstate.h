2501acc9aacd0f4b1d7838fccdb6abb28710b429
Jake Stine <Jake.Stine@gmail.com>
2017-10-07 08:01:10 -0700

    lua: remove longjmp code I added which is not needed (using L->panic instead)

--- imports/lua-5.3.x/src/lstate.h
+++ imports/lua-5.3.x/src/lstate.h
@@ -158,6 +158,9 @@ typedef struct global_State {
 /*
 ** 'per thread' state
 */
+
+typedef void (custom_throw_fn)(int errcode);
+
 struct lua_State {
   CommonHeader;
   unsigned short nci;  /* number of items in 'ci' list */
@@ -173,10 +176,6 @@ struct lua_State {
   struct lua_State *twups;  /* list of threads with open upvalues */
   struct lua_longjmp* errorJmp;  /* current error recover point */
 
-// AJEK-SCRIPT    ----- >
-  jmp_buf* jmpbuf_default;  /* default/universal error recover point if errorJmp is unassigned */
-  int errorStatus;  /* error result set before jumping to jmpbuf_default */
-// <--------------------
   CallInfo base_ci;  /* CallInfo for first level (C calling Lua) */
   volatile lua_Hook hook;
   ptrdiff_t errfunc;  /* current error handling function (stack index) */
