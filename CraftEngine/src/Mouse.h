0edeb41035af151af90b2fe5c770ff221348bea2
Jake Stine <Jake.Stine@gmail.com>
2017-10-30 07:06:50 -0700

    mouse.h: fixed boilerplate
    (note: including dependencies for headers helps reduce odd/inconsistent intellisense behaviors)

--- CraftEngine/src/Mouse.h
+++ CraftEngine/src/Mouse.h
@@ -1,16 +1,19 @@
+#pragma once
+#include "x-simd.h"
+
 class Mouse
 {
     public:
-        
+
         void update();
-        
+
 		bool hasFocus() const { return m_scene_has_focus; }
         bool isInScene() const { return m_mouse_in_scene; }
         float2 getRelativeToCenter() const { return m_mouse_pos_relative_to_center; }
-        
+
     protected:
     private:
-    
+
     bool	m_scene_has_focus = false;
     bool	m_mouse_in_scene = false;
     float2	m_mouse_pos_relative_to_center = {};
