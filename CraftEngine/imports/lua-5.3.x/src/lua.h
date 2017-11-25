5f675c991099508265f66a1cf4c6a94f759abcdd
Jake Stine <Jake.Stine@gmail.com>
2017-11-24 17:43:23 -0800

    lua: normalized whitespace

--- CraftEngine/imports/lua-5.3.x/src/lua.h
+++ CraftEngine/imports/lua-5.3.x/src/lua.h
@@ -366,7 +366,7 @@ LUA_API void      (lua_setallocf) (lua_State *L, lua_Alloc f, void *ud);
 #define lua_pushliteral(L, s)	lua_pushstring(L, "" s)
 
 #define lua_pushglobaltable(L)  \
-	((void)lua_rawgeti(L, LUA_REGISTRYINDEX, LUA_RIDX_GLOBALS))
+    ((void)lua_rawgeti(L, LUA_REGISTRYINDEX, LUA_RIDX_GLOBALS))
 
 #define lua_tostring(L,i)	lua_tolstring(L, (i), NULL)
 
