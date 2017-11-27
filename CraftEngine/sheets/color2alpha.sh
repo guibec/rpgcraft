0f5ad64f3d16edfc19c39463e26091cc179ef3d1
Jake Stine <Jake.Stine@gmail.com>
2017-11-27 12:55:24 -0800

    color2alpha.sh util added - depends on image magick being installed in path

---
+++ CraftEngine/sheets/color2alpha.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# For converting typical opaque 
+
+if [[ -z "$1" || -z "$2" || "$1" == "--help" ]]; then
+	>&2 echo "Convert transparent color into GPU-friendly alpha channel layer."
+	>&2 echo ""
+	>&2 echo "USAGE:"
+	>&2 echo "    $ color2alpha im_color srcimg [destimg]"
+	>&2 echo ""
+	exit 1
+fi
+
+
+color=$1
+infile=$2
+outfile=${3:-$infile}
+
+# use set -x to echo the command for diag purposes...
+( set -x ; magick convert "$infile" -alpha set -channel RGBA -fill none -opaque "$color" "$outfile" )
