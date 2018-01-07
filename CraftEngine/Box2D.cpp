8669f6fd56524da3bd8f8827e14b5c951c3c2c27
Jake Stine <Jake.Stine@gmail.com>
2018-01-06 20:33:40 -0800

    whitespace normalization as performed by git

--- CraftEngine/Box2D.cpp
+++ CraftEngine/Box2D.cpp
@@ -7,9 +7,9 @@ Box2D::~Box2D()
 
 void Box2D::buildFrom(float2 origin, float halfWidth, float halfHeight)
 {
-	m_topLeft = float2(origin.x - halfWidth, origin.y + halfHeight);
-	m_bottomRight = float2(origin.x + halfWidth, origin.y - halfHeight);
-	normalize();
+    m_topLeft = float2(origin.x - halfWidth, origin.y + halfHeight);
+    m_bottomRight = float2(origin.x + halfWidth, origin.y - halfHeight);
+    normalize();
 }
 
 void Box2D::normalize()
