b57fa4ab26defb18e11c181914858e230c4e2cca
rlavalle <robin@guibec.com>
2018-12-08 07:10:27 -0800

    explosion!

--- unity/Assets/Scripts/Game/Player/Bomb.cs
+++ unity/Assets/Scripts/Game/Player/Bomb.cs
@@ -16,44 +16,53 @@ public class Bomb : Entity
     public void Explode()
     {
         // Find all entities near the bomb within a certain radius
-        //foreach (var entity in this.EntitiesWithinRadius(m_explosionRadius))
-        //{
-
-        //}
-    }
-
-    public override void OnTouch(Entity other)
-    {
-        base.OnTouch(other);
-
-        // to prevent another attack while being destroyed
-        // this might need to be moved into the OnTouch collision system ...
-        if (IsDestroying())
-            return;
-
-        Enemy enemy = other as Enemy;
-        if (enemy && (!m_singleHitPerEnemy || !m_hitEntities.ContainsKey(other)))
+        foreach (var entity in CollisionManager.Instance.EntitiesWithinEntityRadius(this, m_explosionRadius))
         {
-            HealthComponent healthComponent = enemy.HealthComponent;
-            if (healthComponent.CanReceiveDamage())
+            // TODO: Shameless copied from SwordAttack - Need to be merged into generic code
+            Enemy enemy = entity as Enemy;
+            if (enemy && (!m_singleHitPerEnemy || !m_hitEntities.ContainsKey(enemy)))
             {
-                // simple damage formula for now
-                int damage = GameManager.Instance.MainPlayer.Experience.Level;
-
-                // check if critical hit !
-                if (RandomManager.Probability(0.10f))
+                HealthComponent healthComponent = enemy.HealthComponent;
+                if (healthComponent.CanReceiveDamage())
                 {
-                    damage *= 3;
+                    // simple damage formula for now
+                    int damage = GameManager.Instance.MainPlayer.Experience.Level;
+
+                    // check if critical hit !
+                    if (RandomManager.Probability(0.10f))
+                    {
+                        damage *= 3;
+                    }
+
+                    healthComponent.ReceiveDamage(damage);
+                    m_hitEntities.Add(enemy, true);
+
+                    Vector2 relativeDir = enemy.transform.position - this.transform.position;
+                    enemy.KnockBack(relativeDir.normalized, 3f, 0.05f);
+
+                    AudioManager.Instance.PlayHit();
                 }
-
-                healthComponent.ReceiveDamage(damage);
-                m_hitEntities.Add(other, true);
-
-                Vector2 relativeDir = other.transform.position - this.transform.position;
-                enemy.KnockBack(relativeDir.normalized, 3f, 0.05f);
-
-                AudioManager.Instance.PlayHit();
             }
         }
     }
+
+    protected override void OnStart()
+    {
+        base.OnStart();
+
+        StartCoroutine(WaitAndExplode());
+    }
+
+    private IEnumerator WaitAndExplode()
+    {
+        yield return new WaitForSeconds(m_numSecondsBeforeExploding);
+        Explode();
+        RequestDestroy();
+    }
+
+    protected override void OnUpdate()
+    {
+        base.OnUpdate();
+    }
+
 }
