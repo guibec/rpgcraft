ceba2970dc9486399ae107fbf7035b4ad41ee770
rlavalle <robin@guibec.com>
2018-12-19 07:55:56 -0800

    WIP

--- unity/Assets/Scripts/Game/Map/BiomeMap.cs
+++ unity/Assets/Scripts/Game/Map/BiomeMap.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class BiomeMap
 {
-    EBiome[,] m_biomes = new EBiome[256, 256];
+    private const int m_width = 256;
+    private const int m_height = 256;
 
+    EBiome[,] m_biomes = new EBiome[m_width, m_height];
+
+    // We build build this based on https://gamedev.stackexchange.com/questions/79049/generating-tile-map
+    // and https://en.wikipedia.org/wiki/Fortune's_algorithm
+    // and http://www-cs-students.stanford.edu/~amitp/game-programming/polygon-map-generation/
+    // 
     public BiomeMap()
     {
+        const int NumNodes = 400;
+
+        // For the purpose of this exercise, we will put all the numbers between 0.0f and 1.0f
+        // we will remap them into the m_biomes after
+
 
     }
 }
