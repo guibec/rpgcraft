187f69419678ec1f5849fb02e6c7257ceef027eb
rlavalle <robin@guibec.com>
2018-12-22 15:51:38 -0800

    WIP biome

--- unity/Assets/Scripts/Engine/Extensions/VectorExtension.cs
+++ unity/Assets/Scripts/Engine/Extensions/VectorExtension.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 public static class VectorExtension
 {
-    public static Vector2[] toVector2Array(this Vector3[] v3)
+    public static Vector2[] ToVector2Array(this Vector3[] v3)
     {
-        return System.Array.ConvertAll<Vector3, Vector2>(v3, getV2fromV3);
+        return System.Array.ConvertAll<Vector3, Vector2>(v3, GetV2fromV3);
     }
 
-    public static Vector3[] toVector3Array(this Vector2[] v2)
+    public static Vector3[] ToVector3Array(this Vector2[] v2)
     {
-        return System.Array.ConvertAll<Vector2, Vector3>(v2, getV3fromV2);
+        return System.Array.ConvertAll<Vector2, Vector3>(v2, GetV3fromV2);
     }
 
-    public static Vector2 getV2fromV3(Vector3 v3)
+    public static Vector2 GetV2fromV3(Vector3 v3)
     {
         return new Vector2(v3.x, v3.y);
     }
 
-    public static Vector3 getV3fromV2(Vector2 v2)
+    public static Vector3 GetV3fromV2(Vector2 v2)
     {
         return new Vector3(v2.x, v2.y, 1.0f);
     }
--- unity/Assets/Scripts/Game/Map/BiomeManager.cs
+++ unity/Assets/Scripts/Game/Map/BiomeManager.cs
@@ -1,13 +1,17 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+
+using IronExtension;
+using Debug = UnityEngine.Debug;
 
 public class BiomeManager
 {
     private const int Width = 64;
     private const int Height = 64;
 
-    private readonly EBiome[,] biomes = new EBiome[Width, Height];
+    private readonly EBiome[,] m_biomes = new EBiome[Width, Height];
 
     List<Vector2> m_points;
 
@@ -16,8 +20,8 @@ public class BiomeManager
 
     public BiomeManager()
     {
-        // Width * Height points randomly on the [0.0 to 1.0 range]
-        const int numPoints = Width * Height;
+        // 32 biome types
+        const int numPoints = 32;
 
         m_points = new List<Vector2>(Width * Height);
 
@@ -27,11 +31,78 @@ public class BiomeManager
             m_points.Add(point);
         }
 
-        m_debugMesh = CreatePointMesh(m_points.ToArray().toVector3Array());
+        m_debugMesh = CreatePointMesh(m_points.ToArray().ToVector3Array());
         m_debugMaterial = new Material(Shader.Find("Sprites/Default"));
 
-        // Then we apply Fortune's algorithm
-        // https://en.wikipedia.org/wiki/Fortune's_algorithm
+        voronoiTesselate();
+    }
+
+    // Implement Voronoi tessellation 
+    // https://en.wikipedia.org/wiki/Voronoi_diagram#/media/File:Voronoi_growth_euclidean.gif
+    private void voronoiTesselate()
+    {
+        Stopwatch sw = Stopwatch.StartNew();
+
+        int[,] regions = new int[Width, Height];
+        Array.Clear(regions, 0, regions.Length);
+
+        // Remap each of the point into the biome. It's possible some will overwrite each other, that's fine, shouldn't happen too much.
+
+        Dictionary<Tuple<int, int>, int> closePoints = new Dictionary<Tuple<int, int>, int>();
+
+        int counter = 1;
+        foreach (Vector2 point in m_points)
+        {
+            int x = (int)(point.x * Width);
+            int y = (int)(point.y * Height);
+
+            // Should be fine since point coordinates are supposed to be [0.0, 1.0[
+            Debug.Assert(x >= 0 && x < Width);
+            Debug.Assert(y >= 0 && y < Height);
+
+            closePoints.Add(new Tuple<int, int>(x, y), counter);
+            regions[x, y] = counter;
+            counter++;
+        }
+
+        // time to fill the missing regions !
+        // I can flood fill to figure out all the regions
+        // or I can do pixel per pixel and figure out which point is the closest
+        // flood fill if done properly will be have run-time of O(Width * Height) + use some memory and is a bit more complex
+        // while pixel per pixel will have O(Width * Height * NumPoints) and is simple.
+        // So we will do pixel per pixel for now, and we can optimize later
+
+        for (int j = 0; j < Height; j++)
+        {
+            for (int i = 0; i < Width; i++)
+            {
+                if (regions[i,j] != 0) // already done
+                {
+                    continue;
+                }
+
+                // not done yet, find the closest point
+                KeyValuePair<Tuple<int, int>, int> best;
+                float bestSqrDistance = float.MaxValue;
+                foreach (var candidate in closePoints)
+                {
+                    Vector2 firstValue = new Vector2(i, j);
+                    Vector2 secondValue = new Vector2(candidate.Key.Item1, candidate.Key.Item2);
+                    float sqrDistance = (firstValue - secondValue).sqrMagnitude;
+
+                    if (sqrDistance < bestSqrDistance)
+                    {
+                        bestSqrDistance = sqrDistance;
+                        best = candidate;
+                    }
+                }
+
+                regions[i, j] = best.Value;
+            }
+        }
+
+        sw.Stop();
+        Debug.Log(string.Format("BiomeManager: Voronoi tessellation took {0} ms", sw.ElapsedMilliseconds));
     }
 
     private Mesh CreatePointMesh(Vector3[] points)
