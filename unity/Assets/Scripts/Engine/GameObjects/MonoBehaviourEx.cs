eb1ecc6fc77daad761d4ab947ed518f131a05cdb
rlavalle <robin@guibec.com>
2018-12-26 08:18:33 -0800

    put dynamic objects into a common hierarchy

--- unity/Assets/Scripts/Engine/GameObjects/MonoBehaviourEx.cs
+++ unity/Assets/Scripts/Engine/GameObjects/MonoBehaviourEx.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class MonoBehaviourEx : MonoBehaviour
 {
+    static GameObject m_dynHolder;
+
     public GameObject Instantiate(GameObject prefab)
     {
-        return MonoBehaviour.Instantiate(prefab);
+        return Instantiate(prefab, Vector3.zero, Quaternion.identity);
     }
 
-    public GameObject  Instantiate(GameObject parent, Vector3 position, Quaternion rotation)
+    public GameObject  Instantiate(GameObject prefab, Vector3 position, Quaternion rotation)
     {
-        return MonoBehaviour.Instantiate(parent, position, rotation);
+        if (m_dynHolder == null)
+        {
+            m_dynHolder = new GameObject("_Dyn");
+            m_dynHolder.transform.SetPositionAndRotation(Vector3.zero, Quaternion.identity);
+        }
+
+        GameObject gameObject = MonoBehaviour.Instantiate(prefab, position, rotation);
+        if (gameObject != null)
+        {
+            gameObject.transform.SetParent(m_dynHolder.transform);
+        }
+
+        return gameObject;
     }
 }
