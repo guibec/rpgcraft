2d723d3e905aacc4812e6ef0d9e7518ce1ab1073
Jake Stine <Jake.Stine@gmail.com>
2019-01-05 16:44:03 -0800

    x-png-decode: better error message report

--- CraftEngine/ajek-framework/h/x-png-decode.h
+++ CraftEngine/ajek-framework/h/x-png-decode.h
@@ -2,8 +2,8 @@
 #pragma once
 
 #include "x-types.h"
+#include "x-string.h"
 #include "x-BitmapData.h"
 
 extern void png_LoadFromFile(xBitmapData& dest, const xString& filename);
-extern void png_LoadFromFile(xBitmapData& dest, FILE* fp);
-
+extern void png_LoadFromFile(xBitmapData& dest, FILE* fp, const xString& filename_for_error_msg_only=xString());
--- CraftEngine/ajek-framework/src/x-png-decode.cpp
+++ CraftEngine/ajek-framework/src/x-png-decode.cpp
@@ -6,33 +6,27 @@
 void png_LoadFromFile(xBitmapData& dest, const xString& filename)
 {
     bug_on (filename.IsEmpty());
-
-    png_image image;
-    png_color background = {0, 0xff, 0}; /* fully saturated green */
-
-    image.version   = PNG_IMAGE_VERSION;
-    image.opaque    = NULL;
-
-    int beginResult = png_image_begin_read_from_file(&image, filename);
-    x_abort_on(!beginResult, "libpng file error: %s", image.message);
-
-    image.format    = PNG_FORMAT_RGBA;
-
-    dest.size = uint2 { image.width, image.height };
-    dest.buffer.Resize(PNG_IMAGE_SIZE(image));
-
-    int finishResult = png_image_finish_read(&image, &background, dest.buffer.GetPtr(),
-        0,          /*row_stride*/
-        NULL        /*colormap for PNG_FORMAT_FLAG_COLORMAP */
-    );
-
-    x_abort_on(!finishResult, "libpng decode error: %s", image.message);
+    auto* fp = xFopen(filename, "rb");
+    x_abort_on (!fp, "png_LoadFromFile('%s') error: %s", filename.c_str(), strerror(errno));
+    png_LoadFromFile(dest, fp, filename);
+    fclose(fp);
 }
 
+static xString png_errmsg_helper(const xString& filename, const FILE* fp)
+{
+    if (filename.IsEmpty()) {
+        return xFmtStr("fp=%s", cPtrStr(fp));
+    }
+    else {
+        return xFmtStr("'%s'", filename.c_str());
+    }
+}
 
-void png_LoadFromFile(xBitmapData& dest, FILE* fp)
+// filename_for_error_msg_only - optional filename parameter for use in error messages only.
+void png_LoadFromFile(xBitmapData& dest, FILE* fp, const xString& filename_for_error_msg_only)
 {
     bug_on (!fp);
+    const xString& filename = filename_for_error_msg_only;
 
     png_image image;
     png_color background = {0, 0xff, 0}; /* fully saturated green */
@@ -41,11 +35,13 @@ void png_LoadFromFile(xBitmapData& dest, FILE* fp)
     image.opaque    = NULL;
 
     int beginResult = png_image_begin_read_from_stdio(&image, fp);
-    x_abort_on(!beginResult, "libpng file error: %s", image.message);
+    x_abort_on(!beginResult, "png_LoadFromFile(%s) read error: %s", png_errmsg_helper(filename, fp), image.message);
+
+    // assume any negative size indicates some invalid object state.
+    dest.size = uint2 { image.width, image.height };
+    x_abort_on(dest.size.cmp_any() < 0, "png_LoadFromFile(%s) error: invalid object state dest.size={ %s }", png_errmsg_helper(filename, fp), cHexStr(dest.size));
 
     image.format    = PNG_FORMAT_RGBA;
-
-    dest.size = uint2 { image.width, image.height };
     dest.buffer.Resize(PNG_IMAGE_SIZE(image));
 
     int finishResult = png_image_finish_read(&image, &background, dest.buffer.GetPtr(),
@@ -53,6 +49,6 @@ void png_LoadFromFile(xBitmapData& dest, FILE* fp)
         NULL        /*colormap for PNG_FORMAT_FLAG_COLORMAP */
     );
 
-    x_abort_on(!finishResult, "libpng decode error: %s", image.message);
+    x_abort_on(!finishResult, "png_LoadFromFile(%s) decode error: %s", png_errmsg_helper(filename, fp), image.message);
 }
 
