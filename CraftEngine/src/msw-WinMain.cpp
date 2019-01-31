guibec/rpgcraft

commit a66b50ca2cbb8daaf691d5b7b2d93304dec63538

logging: get rid of heavyweight 'feature-laden' logging facility
Removes module-scope prefixes, WARN prefixes, and timestamp prefixes.  Most of these things were hold-overs from things other people added to my library as I've been bringing it along with me from project to project... and while I _kind_ of liked the module scope prefix, it was a pain when it came to C++ classes, or printing multi-line text where prefixes interfere with other things.  Adding prefixes manually, as it turns out, is pretty damn easy too.  Easier, in most cases.  So it's gone!

CraftEngine/src/msw-WinMain.cpp
@@ -18,8 +18,6 @@
 
 #include <direct.h>     // for _getcwd()
 
-DECLARE_MODULE_NAME("winmain");
-
 extern void         LogHostInit();
 extern void         MSW_InitChrono();
 extern VirtKey_t    ConvertFromMswVK( UINT key );
@@ -581,7 +579,7 @@ void xOutputStringError(const char* str)
     }
 }
 
-void xOutputString(const char* str)
+void xOutputString(const char* str, FILE* std_fp)
 {
 #if MSW_ENABLE_DEBUG_OUTPUT
     if (::IsDebuggerPresent())

CraftEngine/ajek-framework/h/x-ScopedMalloc.h
@@ -1,7 +1,7 @@
 
 #pragma once
 
-#include "x-stdlib.h"
+#include "x-stdalloc.h"
 #include "x-assertion.h"
 
 // --------------------------------------------------------------------------------------

CraftEngine/ajek-framework/h/x-assertion.h
@@ -205,11 +205,6 @@ enum DbgBreakType
     DbgBreakType_Abort,
 };
 
-enum xLogFlag {
-    xLogFlag_Default        = 0,
-    xLogFlag_Important      = (1 << 0),     // warnings or errors, honored by log_verbose only, also assumes NoRepeat.
-};
-
 struct AssertContextInfoTriad {
     const char*     filepos;
     const char*     funcname;
@@ -220,8 +215,10 @@ struct AssertContextInfoTriad {
 extern void     vlog_append_host_clock  (xString& dest);
 extern void     vlog_append_prefix      (xString& buffer, const char* moduleName);
 
-extern void     _host_log           (uint flags, const char* moduleName, const char* fmt = nullptr, ...)    __verify_fmt(3, 4);
-extern void     _host_log_v         (uint flags, const char* moduleName, const char* fmt, va_list params);
+extern void     log_host           (const char* fmt = nullptr, ...)    __verify_fmt(3, 4);
+extern void     warn_host          (const char* fmt = nullptr, ...)    __verify_fmt(3, 4);
+extern void     log_host_v         (const char* fmt, va_list params);
+extern void     warn_host_v        (const char* fmt = nullptr, ...)    __verify_fmt(3, 4);
 
 extern void     xPrintLn            (const xString& msg);
 extern void     flush_log           ();
@@ -233,33 +230,24 @@ extern void     xStopProcess        ( const char* filepos, const char* funcname 
 extern assert_t xDebugBreak         ( DbgBreakType breakType, const AssertContextInfoTriad& triad, const char* fmt=nullptr, ... ) __verify_fmt(3, 4);
 extern assert_t xDebugBreak_v       ( DbgBreakType breakType, const AssertContextInfoTriad& triad, const char* fmt, va_list list );
 
-// Writes directly to debug console where supported (visual studio), or stdout otherwise.
-extern void     xOutputString       (const char* str);
-
-// Writes directly to debug console where supported (visual studio), or stderr otherwise.
-extern void     xOutputStringError(const char* msg);
-
 extern bool     xIsDebuggerAttached ();
 
-#define log_host(...)                        _host_log      (xLogFlag_Default,      s_ModuleName,           ## __VA_ARGS__)
-#define log_host_loud(...)                   _host_log      (xLogFlag_Important,    s_ModuleName,           ## __VA_ARGS__)
-#define warn_host(...)                       _host_log      (xLogFlag_Important,    s_ModuleName, "WARN: "  ## __VA_ARGS__)
-#define warn_host_on(cond,...)  ((cond) &&  (_host_log      (xLogFlag_Important,    s_ModuleName, "WARN: "  ## __VA_ARGS__), true))
+#define warn_host_on(cond,...)  ((cond) &&  (warn_host      (__VA_ARGS__), true))
 
 #if ENABLE_DEBUG_LOG
-#   define log_debug(fmt, ...)          _host_log( xLogFlag_Default, s_ModuleName, fmt, ## __VA_ARGS__ )
+#   define log_debug(fmt, ...)          log_host( fmt, ## __VA_ARGS__ )
 #else
 #   define log_debug(fmt, ...)          ((void)0)
 #endif
 
 #if ENABLE_QA_LOG
-#   define log_qa(fmt, ...)             _host_log( xLogFlag_Default, s_ModuleName, fmt, ## __VA_ARGS__ )
+#   define log_qa(fmt, ...)             log_host( fmt, ## __VA_ARGS__ )
 #else
 #   define log_qa(fmt, ...)             ((void)0)
 #endif
 
 #if ENABLE_PERF_LOG
-#   define log_perf( fmt, ... )         _host_log( xLogFlag_Default, s_ModuleName, fmt, ## __VA_ARGS__ )
+#   define log_perf( fmt, ... )         log_host( fmt, ## __VA_ARGS__ )
 #else
 #   define log_perf( fmt, ... )         ((void)0)
 #endif
@@ -269,8 +257,8 @@ extern bool     xIsDebuggerAttached ();
 #       define  x_abort(...)                                 (void(_inline_abort_("error/abort", ## __VA_ARGS__)))
 #       define  x_abort_on(cond,...)            ( (cond) &&  (    (_inline_abort_( # cond,       ## __VA_ARGS__)), true) )
 #   else
-#       define  x_abort(...)                                 (_host_log(xLogFlag_Important, "abort",    ## __VA_ARGS__),    __stop())
-#       define  x_abort_on(cond,...)            ( (cond) &&  (_host_log(xLogFlag_Important, "abort",    ## __VA_ARGS__),    __stop(), true) )
+#       define  x_abort(...)                                 (log_host(__VA_ARGS__),    __stop())
+#       define  x_abort_on(cond,...)            ( (cond) &&  (log_host(__VA_ARGS__),    __stop(), true) )
 #   endif
 #   define      abort_var(...)                      __VA_ARGS__
 #else
@@ -290,7 +278,7 @@ extern bool     xIsDebuggerAttached ();
 #if ENABLE_HOST_TRACE_LOGS
 #   define HostTraceEnter()         _verbose_log( 0, __FILEPOS__, __FUNCTION_NAME__, " entered." )
 #   define HostTraceLeave()         _verbose_log( 0, __FILEPOS__, __FUNCTION_NAME__, " completed." )
-#   define HostTraceCheckpoint()    _verbose_log( 0, __FILEPOS__, __FUNCTION_NAME__, " --> Checkpoint Reached" )
+#   define HostTraceCheckpont()    _verbose_log( 0, __FILEPOS__, __FUNCTION_NAME__, " --> Checkpoint Reached" )
 #else
 #   define HostTraceEnter()         ((void)0)
 #   define HostTraceLeave()         ((void)0)
@@ -457,8 +445,8 @@ namespace
 // behavior even in final builds.
 //
 #if ENABLE_WARNING_LOGS
-#   define  warn(msg, ...)                          _host_log( xLogFlag_Important, s_ModuleName, "WARN: " msg,          ## __VA_ARGS__ )
-#   define  warn_on(cond, ...)      ( (cond) && (   _host_log( xLogFlag_Important, s_ModuleName, "WARN: " # cond ", "   ## __VA_ARGS__ ), true) )
+#   define  warn(msg, ...)                          log_host( msg, ## __VA_ARGS__ )
+#   define  warn_on(cond, ...)      ( (cond) && (   log_host( "(warn on) " # cond,  ## __VA_ARGS__ ), true) )
 #else
 #   define  warn(...)               (false)
 #   define  warn_on(cond, ...)      (false)
@@ -502,7 +490,7 @@ inline __ai void xStopProcess(const char* filepos, const char* func)
 {
     // do not call log_abort() here, causes recursion...
     if (ENABLE_ABORT_LOG) {
-        _host_log( xLogFlag_Important, nullptr, filepos, "(function '%s'): Process halted by way of __stop() or similar.", func );
+        log_host("%s: (function '%s') Process halted by way of __stop() or similar.", filepos, func );
     }
 
     abort();

CraftEngine/ajek-framework/h/x-stdalloc.h
@@ -1,0 +1,58 @@
+#pragma once
+
+#include "x-types.h"
+
+// ======================================================================================
+//  xCustomAllocator  (interface)
+// ======================================================================================
+class xCustomAllocator {
+public:
+    virtual void*       alloc       (size_t size)               =0;
+    virtual void*       realloc     (void* ptr, size_t size)    =0;
+    virtual void        free        (void* ptr)                 =0;
+};
+
+class xDefaultCustomAllocator : public xCustomAllocator {
+public:
+    void*       alloc       (size_t size)               override;
+    void*       realloc     (void* ptr, size_t size)    override;
+    void        free        (void* ptr)                 override;
+};
+
+extern xDefaultCustomAllocator  g_DefaultCustomAllocator;
+
+extern void     xMalloc_Check       ();
+extern void*    xMalloc             (size_t sz);
+extern void*    xCalloc             (size_t numItems, size_t sz);
+extern void*    xRealloc            (void* srcptr, size_t sz);
+extern void     xFree               (void *ptr);
+extern void*    xMalloc_Aligned     (size_t sz, u32 align);
+extern void*    xRealloc_Aligned    (void* srcptr, size_t sz, u32 align);
+extern void     xFree_Aligned       (void* ptr);
+extern void     xMalloc_Report      ();
+extern void     xMalloc_ReportDelta ();
+
+#define placement_new(T)        new (xMalloc(sizeof(T))) T
+
+// Performs cleanup of existing object pointer (if non-null) and creates a new object in its place.
+// Memory is allocated if the provided pointer is null.
+template< typename T >
+__xi T* xMallocNew(T* &ptr)
+{
+    if (ptr) {
+        ptr->~T();
+    }
+    else {
+        ptr = (T*)xMalloc(sizeof(*ptr));
+    }
+    return new (ptr) T();
+}
+
+// Memory is allocated if the provided pointer is null.  if pointer is non-null, then no action is taken.
+template< typename T >
+__xi T* xMallocT(T* &ptr)
+{
+    static_assert(!std::has_virtual_destructor<T>::value, "Non-trivial type has meaningful destructor.  Use xMallocNew<T> instead.");
+    //static_assert(!std::is_trivially_copyable<T>::value, "Non-trivial type has meaningful destructor.  Use xMallocNew<T> instead.")
+    return ptr ? ptr : xMalloc(sizeof(*ptr));
+}

CraftEngine/ajek-framework/h/x-stdfile.h
@@ -1,0 +1,112 @@
+#pragma once
+
+#include "x-types.h"
+#include "x-stdalloc.h"
+
+#include <cstdio>
+
+// ======================================================================================
+//  xBaseStream (abstract class)
+// ======================================================================================
+class xBaseStream
+{
+    NONCOPYABLE_OBJECT( xBaseStream );
+
+protected:
+    union {
+        // We cheat her a bit -- m_fd strictly speaking should be -1 for an empty descriptor.
+        // The API manually sets -1 to zero (0) within OpenFile() to allow !nullptr checks
+        // against m_fp to satisfy either underlying file system.  This is ok because posix
+        // fd==0 is reserved as stdin anyway, and will never be returned from open(). --jstine
+
+        FILE*       m_fp;               // non-NULL implies data is sourced from file
+        int         m_fd;
+    };
+
+    size_t              m_dataPos       = 0;
+    size_t              m_dataLength    = 0;
+    bool                m_isBuffered    = false;            // if TRUE, use m_fp else use m_fd
+    xCustomAllocator*   m_allocator     = &g_DefaultCustomAllocator;
+
+protected:
+    xBaseStream() {
+        m_fp            = nullptr;
+    }
+
+public:
+    virtual             ~xBaseStream    () throw()=0;
+    virtual void        Close           ();
+    virtual const void* GetBufferPtr    (size_t size) const=0;
+    virtual void*       GetBufferPtr    (size_t size)=0;
+            x_off_t     Seek            (x_off_t pos, int whence);
+            x_off_t     Tell            ()              const;
+
+            void        SetCustomAlloc  (xCustomAllocator& allocator)   { m_allocator = &allocator; }
+            bool        IsMemory        ()              const   { return  !m_fp;  }
+            bool        IsFile          ()              const   { return !!m_fp; }
+            bool        IsOK            ()              const   { return !!m_fp || !!m_dataLength; }
+            FILE*       GetFILE         ()                      { return m_isBuffered ? m_fp : nullptr; }
+};
+
+inline xBaseStream::~xBaseStream() throw() {}
+
+
+// ======================================================================================
+//  xStreamReader (class)
+// ======================================================================================
+class xStreamReader : public xBaseStream
+{
+    typedef xBaseStream __parent;
+
+protected:
+    const u8*   m_data;
+
+public:
+                        xStreamReader   ()          { }
+    virtual             ~xStreamReader  () throw() { Close(); }
+    virtual void        Close           ()                          override;
+    virtual const void* GetBufferPtr    (size_t size) const         override;
+    virtual void*       GetBufferPtr    (size_t size)               override;
+            bool        OpenFile        ( const xString& filename );
+            bool        OpenFileBuffered( const xString& filename );
+            bool        OpenMem         ( const void* mem, size_t buffer_length );
+            bool        Read            ( void* dest, ssize_t bytes );
+
+    // Valid for both file and data streams in xStreamReader.
+            size_t      GetLength       ()              const   { return m_dataLength; }
+
+    template< typename T >
+    bool Read( T& dest )
+    {
+        serialization_assert(T);
+        return Read( &dest, sizeof(T) );
+    }
+};
+
+struct xStatInfo
+{
+    u32         st_mode;
+    s64         st_size;
+
+    time_t      time_accessed;
+    time_t      time_modified;
+    time_t      time_created;
+
+    bool IsFile     () const;
+    bool IsDir      () const;
+    bool Exists     () const;
+
+};
+
+extern xStatInfo    xFileStat               (const xString& path);
+extern bool         xFileRename             (const xString& src, const xString& dst);
+extern bool         xCreateDirectory        (const xString& dir);
+extern FILE*        xFopen                  (const xString& fullpath, const char* mode);
+
+extern void         xFileSetSize            (int fd, size_t filesize);
+extern bool         xFgets                  (xString& dest, FILE* stream);
+
+
+// Writes directly to debug console where supported (visual studio), or stdout otherwise.
+extern void     xOutputString       (const char* str, FILE* std_fp);
+

CraftEngine/ajek-framework/h/x-stdlib.h
@@ -8,7 +8,6 @@
 
 #include <cstring>      // needed for memset
 #include <type_traits>
-#include <cstdio>
 
 #define serialization_assert(T) \
     static_assert(xIs_trivially_copyable(T), "Cannot serialize non-POD object.")
@@ -52,134 +51,6 @@ public:
     }
 };
 
-
-
-// ======================================================================================
-//  xCustomAllocator  (interface)
-// ======================================================================================
-class xCustomAllocator {
-public:
-    virtual void*       alloc       (size_t size)               =0;
-    virtual void*       realloc     (void* ptr, size_t size)    =0;
-    virtual void        free        (void* ptr)                 =0;
-};
-
-class xDefaultCustomAllocator : public xCustomAllocator {
-public:
-    void*       alloc       (size_t size)               override;
-    void*       realloc     (void* ptr, size_t size)    override;
-    void        free        (void* ptr)                 override;
-};
-
-extern xDefaultCustomAllocator  g_DefaultCustomAllocator;
-
-// ======================================================================================
-//  xBaseStream (abstract class)
-// ======================================================================================
-class xBaseStream
-{
-    NONCOPYABLE_OBJECT( xBaseStream );
-
-protected:
-    union {
-        // We cheat her a bit -- m_fd strictly speaking should be -1 for an empty descriptor.
-        // The API manually sets -1 to zero (0) within OpenFile() to allow !nullptr checks
-        // against m_fp to satisfy either underlying file system.  This is ok because posix
-        // fd==0 is reserved as stdin anyway, and will never be returned from open(). --jstine
-
-        FILE*       m_fp;               // non-NULL implies data is sourced from file
-        int         m_fd;
-    };
-
-    size_t              m_dataPos       = 0;
-    size_t              m_dataLength    = 0;
-    bool                m_isBuffered    = false;            // if TRUE, use m_fp else use m_fd
-    xCustomAllocator*   m_allocator     = &g_DefaultCustomAllocator;
-
-protected:
-    xBaseStream() {
-        m_fp            = nullptr;
-    }
-
-public:
-    virtual             ~xBaseStream    () throw()=0;
-    virtual void        Close           ();
-    virtual const void* GetBufferPtr    (size_t size) const=0;
-    virtual void*       GetBufferPtr    (size_t size)=0;
-            x_off_t     Seek            (x_off_t pos, int whence);
-            x_off_t     Tell            ()              const;
-
-            void        SetCustomAlloc  (xCustomAllocator& allocator)   { m_allocator = &allocator; }
-            bool        IsMemory        ()              const   { return  !m_fp;  }
-            bool        IsFile          ()              const   { return !!m_fp; }
-            bool        IsOK            ()              const   { return !!m_fp || !!m_dataLength; }
-            FILE*       GetFILE         ()                      { return m_isBuffered ? m_fp : nullptr; }
-};
-
-inline xBaseStream::~xBaseStream() throw() {}
-
-
-// ======================================================================================
-//  xStreamReader (class)
-// ======================================================================================
-class xStreamReader : public xBaseStream
-{
-    typedef xBaseStream __parent;
-
-protected:
-    const u8*   m_data;
-
-public:
-                        xStreamReader   ()          { }
-    virtual             ~xStreamReader  () throw() { Close(); }
-    virtual void        Close           ()                          override;
-    virtual const void* GetBufferPtr    (size_t size) const         override;
-    virtual void*       GetBufferPtr    (size_t size)               override;
-            bool        OpenFile        ( const xString& filename );
-            bool        OpenFileBuffered( const xString& filename );
-            bool        OpenMem         ( const void* mem, size_t buffer_length );
-            bool        Read            ( void* dest, ssize_t bytes );
-
-    // Valid for both file and data streams in xStreamReader.
-            size_t      GetLength       ()              const   { return m_dataLength; }
-
-    template< typename T >
-    bool Read( T& dest )
-    {
-        serialization_assert(T);
-        return Read( &dest, sizeof(T) );
-    }
-};
-
-struct xStatInfo
-{
-    u32         st_mode;
-    s64         st_size;
-
-    time_t      time_accessed;
-    time_t      time_modified;
-    time_t      time_created;
-
-    bool IsFile     () const;
-    bool IsDir      () const;
-    bool Exists     () const;
-
-};
-
-
-extern void     xMalloc_Check       ();
-extern void*    xMalloc             (size_t sz);
-extern void*    xCalloc             (size_t numItems, size_t sz);
-extern void*    xRealloc            (void* srcptr, size_t sz);
-extern void     xFree               (void *ptr);
-extern void*    xMalloc_Aligned     (size_t sz, u32 align);
-extern void*    xRealloc_Aligned    (void* srcptr, size_t sz, u32 align);
-extern void     xFree_Aligned       (void* ptr);
-extern void     xMalloc_Report      ();
-extern void     xMalloc_ReportDelta ();
-
-#define placement_new(T)        new (xMalloc(sizeof(T))) T
-
 template<typename T>
 void placement_delete(T* ptr)   { (ptr)->~T(); xFree(ptr); }
 
@@ -195,41 +66,9 @@ extern void         xMemCopyQwc_WrappedSrc  (u128* dest,        const u128* srcB
 extern void         xMemCopyShortQwc        (void* dest, const void* src, uint lenQwc);
 extern void         xMemCopyShortQwc_NT     (void* dest, const void* src, uint lenQwc);
 
-extern xStatInfo    xFileStat               (const xString& path);
-extern bool         xFileRename             (const xString& src, const xString& dst);
-extern bool         xCreateDirectory        (const xString& dir);
-extern FILE*        xFopen                  (const xString& fullpath, const char* mode);
-
-extern void         xFileSetSize            (int fd, size_t filesize);
-extern bool         xFgets                  (xString& dest, FILE* stream);
-
 extern bool         xEnvironExists          (const xString& varname);
 extern xString      xEnvironGet             (const xString& varname);
 extern void         xEnvironSet             (const xString& varname, const xString& value, bool overwrite=1);
-
-
-// Performs cleanup of existing object pointer (if non-null) and creates a new object in its place.
-// Memory is allocated if the provided pointer is null.
-template< typename T >
-__xi T* xMallocNew(T* &ptr)
-{
-    if (ptr) {
-        ptr->~T();
-    }
-    else {
-        ptr = (T*)xMalloc(sizeof(*ptr));
-    }
-    return new (ptr) T();
-}
-
-// Memory is allocated if the provided pointer is null.  if pointer is non-null, then no action is taken.
-template< typename T >
-__xi T* xMallocT(T* &ptr)
-{
-    static_assert(!std::has_virtual_destructor<T>::value, "Non-trivial type has meaningful destructor.  Use xMallocNew<T> instead.");
-    //static_assert(!std::is_trivially_copyable<T>::value, "Non-trivial type has meaningful destructor.  Use xMallocNew<T> instead.")
-    return ptr ? ptr : xMalloc(sizeof(*ptr));
-}
 
 template< typename T >
 inline void xMemMove(T* dest, size_t destLen, const T* src, size_t srcLen)

CraftEngine/ajek-framework/h/x-types.h
@@ -899,10 +899,6 @@ inline __ai bool IsInInt32u(u64 x)  { return (x <= 0xFFFFFFFFUL); }
 #   define __FUNCTION_NAME__        ((const char*)__FUNCTION__)
 #endif
 
-#define DECLARE_MODULE_NAME_CLASS( name )   static      __unused const char* const s_ModuleName;
-#define DECLARE_MODULE_NAME_LOCAL( name )   static      __unused const char* const s_ModuleName = name;
-#define DECLARE_MODULE_NAME( name )         namespace { __unused const char* const s_ModuleName = name; }
-
 // -------------------------------------------------------------
 //                     C++ Class Macros
 // -------------------------------------------------------------

CraftEngine/ajek-framework/msbuild/ajek-framework.vcxproj
@@ -110,6 +110,8 @@
     <ClInclude Include="..\h\x-png-encode.h" />
     <ClInclude Include="..\h\x-ScopedMalloc.h" />
     <ClInclude Include="..\h\x-simd.h" />
+    <ClInclude Include="..\h\x-stdalloc.h" />
+    <ClInclude Include="..\h\x-stdfile.h" />
     <ClInclude Include="..\h\x-stdlib.h" />
     <ClInclude Include="..\h\x-stl.h" />
     <ClInclude Include="..\h\x-string.h" />

commit 45bc7f09ac0c3d387fe6c4f2dc6cb9532e7f874e

Fix bug in xOutputString which was introduced by PR #80

CraftEngine/src/msw-WinMain.cpp
@@ -579,18 +579,6 @@ bool xIsDebuggerAttached()
     return ::IsDebuggerPresent();
 }
 
-void xOutputStringError(const char* str)
-{
-#if MSW_ENABLE_DEBUG_OUTPUT
-    if (::IsDebuggerPresent())
-        OutputDebugStringA(str);
-    else
-#endif
-    {
-        fputs(str, stderr);
-    }
-}
-
 void xOutputString(const char* str, FILE* std_fp)
 {
 #if MSW_ENABLE_DEBUG_OUTPUT
@@ -599,7 +587,7 @@ void xOutputString(const char* str, FILE* std_fp)
     else
 #endif
     {
-        fputs(str, stdout);
+        fputs(str, std_fp);
     }
 }
 