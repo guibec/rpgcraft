9d3ab45c0393f0e7f0bcefacda9815ab1f3404f5
Jake Stine <Jake.Stine@gmail.com>
2019-02-06 15:08:15 -0800

    dx11: dynamic vertex buffer diagnostics - naming and sanity checking

--- CraftEngine/ajek-gpu/h/x-gpu-ifc.h
+++ CraftEngine/ajek-gpu/h/x-gpu-ifc.h
@@ -260,7 +260,7 @@ extern void                 dx11_CleanupDevice              ();
 extern void                 dx11_NewFrame                   ();
 extern void                 dx11_BeginFrameDrawing          ();
 extern void                 dx11_SubmitFrameAndSwap         ();
-extern void                 dx11_CreateDynamicVertexBuffer  (GPU_DynVsBuffer& dest, int bufferSizeInBytes);
+extern void                 dx11_CreateDynamicVertexBuffer  (GPU_DynVsBuffer& dest, int bufferSizeInBytes, const char* diag_name=nullptr);
 extern void                 dx11_CreateStaticMesh           (GPU_VertexBuffer&  dest, void* vertexData, int itemSizeInBytes, int vertexCount);
 extern void                 dx11_CreateIndexBuffer          (GPU_IndexBuffer&   dest, void* indexBuffer, int bufferSize);
 extern void                 dx11_CreateConstantBuffer       (GPU_ConstantBuffer& dest, int bufferSize);
